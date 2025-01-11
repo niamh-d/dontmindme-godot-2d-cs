9722b961ddd093c99c8718e4cc1bfc8874a9379a
Niamh <niamh.linguist@gmail.com>
2025-01-12 01:13:24 +0200

    npc is patrolling

--- Scenes/NPC/Npc.cs
+++ Scenes/NPC/Npc.cs
@@ -1,12 +1,37 @@
+using System;
+using System.Collections.Generic;
 using Godot;
 
 public partial class Npc : CharacterBody2D
 {
 	[Export] private NavigationAgent2D _navAgent;
 	[Export] private float _moveSpeed = 100.0f;
+	[Export] private Node2D _patrolPoints;
+
+	private enum EnemyState { Patrolling, Chasing, Searching }
+	private EnemyState _state = EnemyState.Patrolling;
+	private int _currentWp = 0;
+	private List<Vector2> _waypoints = new();
 
 	public override void _Ready()
 	{
+		SetPhysicsProcess(false);
+		CreateWaypoints();
+		CallDeferred(MethodName.LateSetup);
+	}
+
+	private async void LateSetup()
+	{
+		await ToSignal(GetTree(), SceneTree.SignalName.PhysicsFrame);
+		SetPhysicsProcess(true);
+	}
+
+	private void CreateWaypoints()
+	{
+		foreach (Node2D child in _patrolPoints.GetChildren())
+		{
+			_waypoints.Add(child.GlobalPosition);
+		}
 	}
 
 	public override void _PhysicsProcess(double delta)
@@ -15,6 +40,7 @@ public partial class Npc : CharacterBody2D
 		{
 			_navAgent.TargetPosition = GetGlobalMousePosition();
 		}
+		UpdateMovement();
 		UpdateNavigation();
 		UpdateDebugLabel();
 	}
@@ -38,5 +64,50 @@ public partial class Npc : CharacterBody2D
 			Velocity = Position.DirectionTo(nextPathPos) * _moveSpeed;
 			MoveAndSlide();
 		}
+	}
+
+	private void SetNextWaypoint()
+	{
+		_currentWp %= _waypoints.Count;
+		_navAgent.TargetPosition = _waypoints[_currentWp];
+		_currentWp++;
+	}
+
+	private void UpdateMovement()
+	{
+		switch (_state)
+		{
+			case EnemyState.Patrolling:
+				ProcessPatrolling();
+				break;
+			case EnemyState.Chasing:
+				ProcessChasing();
+				break;
+			case EnemyState.Searching:
+				ProcessSearching();
+				break;
+			default:
+				ProcessPatrolling();
+				break;
+		}
+	}
+
+	private void ProcessPatrolling()
+	{
+		if (_navAgent.IsNavigationFinished())
+		{
+			SetNextWaypoint();
+			_state = EnemyState.Patrolling;
+		}
+	}
+
+	private void ProcessChasing()
+	{
+		throw new NotImplementedException();
+	}
+
+	private void ProcessSearching()
+	{
+		throw new NotImplementedException();
 	}
 }
